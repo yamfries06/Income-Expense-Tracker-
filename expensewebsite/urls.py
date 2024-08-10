cbe7bc69fdf9e89e1d4f86a1e2e8039f5cf98c1c
yan <yanyam.xue@gmail.com>
2024-08-10 01:02:02 -0700

    got the website running with a basic side base

--- expensewebsite/urls.py
+++ expensewebsite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for expense_tracker project.
+URL configuration for expensewebsite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('', include('expenses.urls')),
     path('admin/', admin.site.urls),
     path('', views.index, name='index')
 ]
--- expensewebsite/views.py
+++ expensewebsite/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render 
 
 def index(request): 
+    print("rendering index view")
     return render(request, "index.html")
